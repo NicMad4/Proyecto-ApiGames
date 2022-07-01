272e9b39f2e729588e66989e07a76ccda6c94c04
CristianMP04 <christian.marti.pernin@gmail.com>
2022-07-01 16:01:58 +0200

    Update ApiHandler.js
    he añadido el key api

--- services/ApiHandler.js
+++ services/ApiHandler.js
@@ -4,11 +4,11 @@ const axios = require('axios');
 class CharactersApi {
   constructor() {
     this.api = axios.create({
-      baseURL: 'https://rickandmortyapi.com/api'
+      baseURL: 'https://api.rawg.io/api/platforms?key=83a729a4b653492d8e187bdbcdd29d06'
     });
   }
 
-  getAllCharacters = () => this.api.get('/character');
+  getAllCharacters = () => this.api.get("/"); //Aqui va el model
 
 }
 
