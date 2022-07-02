be4c65d8f83c65c0808102b0c85495a3ac3465cc
CristianMP04 <christian.marti.pernin@gmail.com>
2022-07-02 12:44:12 +0200

    Model añadido
    Model añadido

--- models/Character.model.js
+++ models/Character.model.js
@@ -1,14 +1,18 @@
 const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
-const characterSchema = new Schema(
+const gameSchema = new Schema(
   {
-    name: String,
-    status: String,
-    species: String,
-    gender: String,
-    image: String,
-    apiId: Number
+    id: Number,
+    title: String,
+    thumbnail: String,
+    short_description: String,
+    game_url: String,
+    genre: Number,
+    platform: String,
+    developer: String,
+    release_date: String,
+    freetogame_profile_url: String
   },
   {
     timestamps: true,
@@ -16,7 +20,7 @@ const characterSchema = new Schema(
 );
 
 
-characterSchema.pre("save", function(next) {
+gameSchema.pre("save", function(next) {
   // console.log(this)
 
   const nameToUpper = this.name.split(' ').map(word => word[0].toUpperCase() + word.slice(1).toLowerCase()).join(' ')
@@ -29,4 +33,4 @@ characterSchema.pre("save", function(next) {
 
 // const Character = model("Character", userSchema);
 
-module.exports = model("Character", characterSchema);
+module.exports = model("Games", gameSchema);
